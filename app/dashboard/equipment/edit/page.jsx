80de96200fdd24a6da6c63bf7dbf945fe1e584f4
arcosane <castelinoaston123@gmail.com>
2023-10-15 10:42:23 +0530

    modified the add item list

--- app/dashboard/equipment/edit/page.jsx
+++ app/dashboard/equipment/edit/page.jsx
@@ -10,6 +10,7 @@ const Form = () => {
   const [inDate, setInDate] = useState('')
   const [quantity, setQuantity] = useState('')
   const [room, setRoom] = useState('')
+  const [expDate, setexpDate] = useState('')
 
   const handleSubmit = async (event) => {
     event.preventDefault()
@@ -33,6 +34,7 @@ const Form = () => {
       isLoaned: false,
       date: inDate + 'T00:00:00.000Z',
       status: 'none',
+      expDate: expDate + 'T00:00:00.000Z',
     }
 
     try {
@@ -42,6 +44,7 @@ const Form = () => {
       setItemName('')
       setBrand('')
       setInDate('')
+      setExpDate('')
       setQuantity('')
       setRoom('')
     } catch (err) {
@@ -65,61 +68,85 @@ const Form = () => {
               Item Name
             </label>
             <input
-              className="mb-4 block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
+              className="mb-2 block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
               id="entry1"
               type="text"
               value={itemName}
               onChange={(e) => setItemName(e.target.value)}
             />
             <label
-              className="text-072140 mb-2 block font-mono font-bold"
+              className="mb-2 text-072140 block font-mono font-bold"
               htmlFor="entry2"
             >
               Brand
             </label>
             <input
-              className="mb-4 block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
+              className="mb-2 block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
               id="entry2"
               type="text"
               value={brand}
               onChange={(e) => setBrand(e.target.value)}
             />
+            <label
+              className="mb-2 text-072140 mb-2 block font-mono font-bold"
+              htmlFor="select"
+            >
+              Item-Category
+            </label>
+            <select className="mb-2 select select-bordered border-gray-500 w-full max-w-xs font-mono text-black" id="select">
+              <option disabled selected>Item-Category?</option>
+              <option>Consumable</option>
+              <option>Non-Consumable</option>
+            </select>
+            <label
+              className="mb-2 text-072140 block font-mono font-bold"
+              htmlFor="expDate"
+            >
+              Exp-Date
+            </label>
+            <input
+              className="mb-2 block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
+              id="expDate"
+              type="date"
+              value={expDate}
+              onChange={(e) => setexpDate(e.target.value)}
+            />
           </div>
           <div>
             <label
-              className="text-072140 mb-2 block font-mono font-bold"
+              className="mb-2 text-072140 mb-2 block font-mono font-bold"
               htmlFor="entry4"
             >
               In-Date
             </label>
             <input
-              className="block w-full rounded border border-black px-4 py-2 font-mono text-black"
+              className="mb-2 block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
               id="entry4"
               type="date"
               value={inDate}
               onChange={(e) => setInDate(e.target.value)}
             />
             <label
-              className="text-072140 mb-2 block font-mono font-bold"
+              className="mb-2 text-072140 mb-2 block font-mono font-bold"
               htmlFor="entry5"
             >
               Quantity
             </label>
             <input
-              className="block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
+              className="mb-2 block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
               id="entry5"
               type="number"
               value={quantity}
               onChange={(e) => setQuantity(e.target.value)}
             />
             <label
-              className="text-072140 mb-2 block font-mono font-bold"
+              className="mb-2 text-072140 mb-2 block font-mono font-bold"
               htmlFor="entry6"
             >
               Room
             </label>
             <input
-              className="block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
+              className="mb-2 block w-full rounded border border-gray-500 px-4 py-2 font-mono text-black"
               id="entry6"
               type="text"
               value={room}
@@ -127,7 +154,7 @@ const Form = () => {
             />
           </div>
         </div>
-        <div className="flex justify-center">
+        <div className="mb-2 flex justify-center">
           <button
             className="bg-072140 mt-4 rounded border border-black px-4 py-2 font-bold text-white hover:bg-blue-700"
             type="submit"
