f2e2f494e7020f8dcec09d3d58aa65ff0c30c484
falcaozane <falcaozane@gmail.com>
2024-03-17 16:59:15 +0530

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,9 +4,11 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <div className="bottom-0 w-full">
-      <footer className="bg-white dark:bg-gray-900 items-center">
-        <h1>Made by GDSC CRCE</h1>
-      </footer>
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+      </aside>
+    </footer>
     </div>
   )
 }
