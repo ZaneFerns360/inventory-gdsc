d72f5dc27fb6d95e5c3c3526e163cfd5c88fe026
arcosane <castelinoaston123@gmail.com>
2024-03-17 17:02:35 +0530

    Tailwind Confug

--- app/dashboard/rooms/page.jsx
+++ app/dashboard/rooms/page.jsx
@@ -1,16 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
-
 export default function page() {
   return (
     <div>
       <div className="collapse bg-base-200">
         <input type="radio" name="my-accordion-1" defaultChecked /> 
         <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+          Humanities & Sciences
         </div>
-        <div className="collapse-content"> 
-          <p>hello</p>
+        <div className="collapse-content flex flex-col"> 
+          <Link href="#">Physics Lab</Link>
+          <Link href="#">Chemistry Lab</Link>
+
         </div>
+        
       </div>
       <div className="collapse bg-base-200">
         <input type="radio" name="my-accordion-1" /> 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/flowbite-react/**/*.js',
   ],
   theme: {
     extend: {
@@ -20,8 +19,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("flowbite/plugin","daisy")],
+  plugins: [require('daisyui')],
   daisyui: {
-    themes: ["light","flowbite/plugin"],
+    themes: ["light"],
   },
 }
