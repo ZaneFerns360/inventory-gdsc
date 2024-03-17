95b40a8d9712fd9e7413e72d09975dc7116e6fc3
falcaozane <falcaozane@gmail.com>
2024-03-17 17:03:50 +0530

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <div className="bottom-0 w-full">
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content text-center bg-slate-200">
+    <footer className="footer footer-center p-4 text-base-content text-center bg-slate-200">
       <aside> 
         <p>Copyright © 2024 - All right reserved by Fr CRCE</p>
         <p>Made by GDSC-CRCE</p>
